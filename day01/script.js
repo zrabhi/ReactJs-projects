37fc2b3d9665e030b3c8b67816574a10dccd258d
ZAKARIA RABHI <zac.rabhi12@gmail.com>
2023-06-09 02:45:08 +0100

    simple example clone ussing (className and conponemt)

--- day01/script.js
+++ day01/script.js
@@ -2,8 +2,7 @@
 
 const div = document.getElementById("root");
 
-function MainDiv() 
-{
+function MainDiv() {
   return (<div id="test"></div>);
 }
 
@@ -16,7 +15,7 @@ function MainContent() {
   );
 }
 
-const NavbarSTYLE = 
+const NavbarSTYLE =
 {
   diplay: 'flex',
   // flexDirection: 'row',
@@ -26,22 +25,57 @@ const NavbarSTYLE =
   fontSize: '1.5rem',
 };
 
-const liStyle = 
+const liStyle =
 {
-  flex:'1',
+  flex: '1',
   flexDirection: 'row',
 }
 
-const avbar = (
-  <nav style = {NavbarSTYLE}>
-    <h1>bobs's bostro </h1>
-    <ul style= {liStyle}>
-      <li >Home</li>
-      <li >Menu</li>
-      <li >About Us</li>
-      <li >Contact</li>
-    </ul>
-  </nav>
-);
+const HeaderStyle = {
+  alignItems: 'center',
+  backgroundColor: '#61DBFB',
+  color: 'black',
+  width:'100%',
+}
+//jsx header element
 
-ReactDOM.render(avbar, div);
+const header = (
+  <header style={HeaderStyle}>
+    <div className="header-wrapper">
+      <h1>Welcome to 30 days of React</h1>
+      <h2>Getting started React</h2>
+      <h3>Javascript library</h3>
+      <p>Zac rabhi</p>
+      <smal>June 9, 2023</smal>
+    </div>
+  </header>
+)
+
+const mainStyle = {
+  backgroundColor: '#F3F0F5',
+  width:'100%',
+}
+
+const main = (
+
+  <main style={mainStyle}>
+      <p>Prerequisite to get started {' '} 
+        <strong>
+            reactJs
+        </strong> 
+      </p>
+      <ul>
+        <li>Html</li>
+        <li>Css</li>
+        <li>JavaScript</li>
+      </ul>
+  </main>
+)
+
+const app = (
+  <div className="app">
+    {header}
+    {main}
+  </div>
+)
+ReactDOM.render(app, div);
