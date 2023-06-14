7da9245500c608ce19477a4b71fa4d20b9b273e0
ZAKARIA RABHI <zac.rabhi12@gmail.com>
2023-06-14 03:08:37 +0100

    day is finished

--- day04/level1/app/src/App.js
+++ day04/level1/app/src/App.js
@@ -1,45 +1,43 @@
-import './App.css';
+import "./App.css";
 import zrabhiImage from "./images/zrabhi.jpg";
 import verIcon from "./images/check.png";
 import timeIcon from "./images/clock.png";
 
+const techs = [
+  "HTML",
+  "CSS",
+  "Sass",
+  "JS",
+  "React",
+  "Redux",
+  "Node",
+  "MongoDB",
+  "Pyhton",
+  "Flask",
+  "Django",
+  "NumPy",
+  "Pandas",
+  "Data analysis",
+  "MYSQL",
+  "GraphQL",
+  "D3.js",
+  "Gatsby",
+  "Docker",
+  "Heroku",
+  "Git",
+  "C/C++",
+  "TypeScript",
+  "Express",
+  "Nest",
+];
 
-function SkillsList() {
-  const techs = [
-    "HTML",
-    "CSS",
-    "Sass",
-    "JS",
-    "React",
-    "Redux",
-    "Node",
-    "MongoDB",
-    "Pyhton",
-    "Flask",
-    "Django",
-    "NumPy",
-    "Pandas",
-    "Data analysis",
-    "MYSQL",
-    "GraphQL",
-    "D3.js",
-    "Gatsby",
-    "Docker",
-    "Heroku",
-    "Git",
-    "C/C++",
-    "TypeScript",
-    "Express",
-    "Nest",
-  ];
-  const techsFormatted = techs.map((tech) => <li key={tech}>{tech}</li>);
+function SkillsList({Techs}) {
+  const techsFormatted = Techs.map((tech) => <li key={tech}>{tech}</li>);
   return techsFormatted;
 }
 
-// const TimeNow = "June 11, 2023";
-
 function DateNow(props) {
-  const {TimeNow} = props.data;
+  const { TimeNow } = props.data;
   return (
     <div className="Date">
       <img className="timeIcon" src={timeIcon} alt="time joined" />
@@ -48,26 +46,32 @@ function DateNow(props) {
   );
 }
 
-const App = (props) =>
-{
+const App = (props) => {
   const data = {
-    TimeNow:'June 11, 2023',
-  }
-  const {firstName, lastName, Job, Country} = props.data.author;
+    TimeNow: "June 11, 2023",
+  };
+  const { firstName, lastName, Job, Country } = props.data.author;
   return (
-  <div className="App">
+    <div className="App">
       <img className="MainImage" src={zrabhiImage} alt="zac rabhi" />
-    <div className="MainInfo">
-      <h1>{firstName}{' '}{lastName}</h1>
-      <img className="Icon" src={verIcon} alt="verified" />
+      <div className="MainInfo">
+        <h1>
+          {firstName} {lastName}
+        </h1>
+        <img className="Icon" src={verIcon} alt="verified" />
+      </div>
+      <h2>
+        {Job}
+        {", "}
+        {Country}
+      </h2>
+      <h3>SKILLS</h3>
+      <ul className="Skills">
+        <SkillsList Techs={techs}/>
+      </ul>
+      <DateNow data={data} />
     </div>
-    <h2>{Job}{', '}{Country}</h2>
-    <h3>SKILLS</h3>
-    <ul className="Skills">
-      <SkillsList />
-    </ul>
-    <DateNow data={data}/>
-  </div>);
-}
+  );
+};
 
 export default App;
--- day04/level1/app/src/index.js
+++ day04/level1/app/src/index.js
@@ -1,23 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import './index.css';
+import "./index.css";
 import App from "./App.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 const data = {
-    author:{
-      firstName:'ZAKARIA',
-      lastName:'RABHI', 
-      Job:'Senior Devloper',
-      Country:'Morocco',
-    }
-}
+  author: {
+    firstName: "ZAKARIA",
+    lastName: "RABHI",
+    Job: "Senior Devloper",
+    Country: "Morocco",
+  },
+};
+
 root.render(
   <React.StrictMode>
-    <App data={data}/>
+    <App data={data} />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
