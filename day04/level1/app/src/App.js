771206d952b56fc1d29dfc4eb5fedbaef5a60700
ZAKARIA RABHI <zac.rabhi12@gmail.com>
2023-06-14 02:00:39 +0100

    reimpleminting level1 with props

--- day04/level1/app/src/App.js
+++ day04/level1/app/src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import zrabhiImage from "./images/zrabhi.jpg";
+import verIcon from "./images/check.png";
+import timeIcon from "./images/clock.png";
 
-function App() {
+
+function SkillsList() {
+  const techs = [
+    "HTML",
+    "CSS",
+    "Sass",
+    "JS",
+    "React",
+    "Redux",
+    "Node",
+    "MongoDB",
+    "Pyhton",
+    "Flask",
+    "Django",
+    "NumPy",
+    "Pandas",
+    "Data analysis",
+    "MYSQL",
+    "GraphQL",
+    "D3.js",
+    "Gatsby",
+    "Docker",
+    "Heroku",
+    "Git",
+    "C/C++",
+    "TypeScript",
+    "Express",
+    "Nest",
+  ];
+  const techsFormatted = techs.map((tech) => <li key={tech}>{tech}</li>);
+  return techsFormatted;
+}
+
+const TimeNow = "June 11, 2023";
+
+function DateNow() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Date">
+      <img className="timeIcon" src={timeIcon} alt="time joined" />
+      <h1 className="Joined">Joined on {TimeNow}</h1>
     </div>
   );
+}
+
+const App = (props) =>
+{
+  const {firstName, lastName, Job, Country} = props.data.author;
+  return (
+  <div className="App">
+      <img className="MainImage" src={zrabhiImage} alt="zac rabhi" />
+    <div className="MainInfo">
+      <h1>{firstName}{' '}{lastName}</h1>
+      <img className="Icon" src={verIcon} alt="verified" />
+    </div>
+    <h2>{Job}{', '}{Country}</h2>
+    <h3>SKILLS</h3>
+    <ul className="Skills">
+      <SkillsList />
+    </ul>
+    <DateNow />
+  </div>);
 }
 
 export default App;
--- day04/level1/app/src/index.js
+++ day04/level1/app/src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from "./App.js";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const data = {
+    author:{
+      firstName:'ZAKARIA',
+      lastName:'RABHI', 
+      Job:'Senior Devloper',
+      Country:'Morocco',
+    }
+}
 root.render(
   <React.StrictMode>
-    <App />
+    <App data={data}/>
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
