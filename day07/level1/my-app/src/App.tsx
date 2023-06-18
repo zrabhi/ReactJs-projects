93029eff24a542c767e9dfecf4968287ed35dc8a
ZAKARIA RABHI <zac.rabhi12@gmail.com>
2023-06-19 00:33:30 +0100

    level almost finished (style left)

--- day07/level1/my-app/src/App.tsx
+++ day07/level1/my-app/src/App.tsx
@@ -15,60 +15,101 @@ type data = {
   date: string;
 };
 
-type ButtonProps = 
-{
+type ButtonProps = {
   text: string;
   onClick: () => void;
+  style: React.CSSProperties;
 };
 
-type MainContentProp = {
-
-}
+type MainContentProp = {};
 
 type HeaderContentProp = {
   data: data;
-}
-class HeaderContent extends Component<HeaderContentProp> {
- 
-  render() 
-    {
-      const { Welcome, Title, Subtitle } = this.props.data;
-      const { FirstName, LastName } = this.props.data.author;
+};
 
-      return (
-        <div className="MainContetent">
-          <h1>{Welcome}</h1>
-          <h2>{Title}</h2>
-          <h3>{Subtitle}</h3>
-          <p>
-            {FirstName} {LastName}
-          </p>
-        </div>
-      );
-    }
+type userProps = {
+  FirstName: string;
+  LastName: string;
+  Image: string;
+};
+
+type FooterProps = {
+  date: string;
+};
+
+type CountProps = {
+  count: number;
+  addOne: () => void;
+  minusOne: () => void;
+};
+
+const buttonStyles: React.CSSProperties = {
+  backgroundColor: "#61dbfb",
+  padding: 10,
+  border: "none",
+  borderRadius: 5,
+  margin: 3,
+  cursor: "pointer",
+  fontSize: 18,
+  color: "white",
+};
+
+class HeaderContent extends Component<HeaderContentProp> {
+  render() {
+    const { Welcome, Title, Subtitle } = this.props.data;
+    const { FirstName, LastName } = this.props.data.author;
+
+    return (
+      <div className="MainContetent">
+        <h1>{Welcome}</h1>
+        <h2>{Title}</h2>
+        <h3>{Subtitle}</h3>
+        <p>
+          {FirstName} {LastName}
+        </p>
+      </div>
+    );
   }
+}
 
 class techList extends Component {
-  render()
-  {
-    const techs: string[] = ['Html', 'Css', 'JavaScript'];
-    const techsFormatted = techs.map((tech) => <li key={tech}>{tech}</li>) 
-    return (techsFormatted);
+  render() {
+    const techs: string[] = ["Html", "Css", "JavaScript"];
+    const techsFormatted = techs.map((tech) => <li key={tech}>{tech}</li>);
+    return techsFormatted;
   }
 }
-class MainContent extends Component<MainContentProp>
-{
-  buttonStyles = {
-    backgroundColor: '#61dbfb',
-    padding: 10,
-    border: 'none',
-    borderRadius: 5,
-    margin: 3,
-    cursor: 'pointer',
-    fontSize: 18,
-    color: 'white',
+
+const Button: React.FC<ButtonProps> = ({ text, onClick, style }) => {
+  return (
+    <button style={style} onClick={onClick}>
+      {text}
+    </button>
+  );
 };
-}
+
+const UserCard: React.FC<userProps> = ({ FirstName, LastName, Image }) => {
+  return (
+    <div className="user-card">
+      <img src={Image} alt={FirstName} />
+      <h2>
+        {FirstName}
+        {LastName}
+      </h2>
+    </div>
+  );
+};
+
+const Footer: React.FC<FooterProps> = ({ date }) => {
+  return (
+    <footer>
+      <div className="footer-wrapper"></div>
+      <p>Copyrigh {date}</p>
+    </footer>
+  );
+};
+
+class MainContent extends Component<MainContentProp> {}
 
 export class App extends Component {
   state = {
@@ -103,7 +144,15 @@ export class App extends Component {
   greetPeople = () => {
     alert("Welcome to 30 days of react, 2023");
   };
-  changeBackground = () => {};
+  changeBackground = () => {
+    let WhiteBackGround = "white";
+    let BlackBackGround = "Black";
+    let CurrentColor =
+      this.state.styles.backgroundColor === WhiteBackGround
+        ? BlackBackGround
+        : WhiteBackGround;
+    this.setState({CurrentColor});
+  };
   render() {
     const UserData: data = {
       Welcome: "Welcome to 30 Days Of React",
@@ -121,6 +170,23 @@ export class App extends Component {
       <div className="header-wrapper">
         {this.state.styles.backgroundColor}
         <HeaderContent data={UserData} />
+        <UserCard FirstName="Zakaria" LastName="rabhi" Image={zrabhiImage} />
+        <Button
+          text="Greet People"
+          style={buttonStyles}
+          onClick={this.greetPeople}
+        />
+        <Button
+          text="Show Time"
+          style={buttonStyles}
+          onClick={this.handleTime}
+        />
+        <Button
+          text="Change Background"
+          style={buttonStyles}
+          onClick={this.changeBackground}
+        />
+        <Footer date={this.showDate(new Date())} />
       </div>
     );
   }
